2c4dbbb85c46ce2f3c796022ef852d14490ad4ce
Xavier Arbat Marquez <xavieram@ext.bershka.com>
2022-02-11 10:34:01 +0100

    second commit

---
+++ app/src/main/java/com/example/draganddropemojis/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.example.draganddropemojis
+
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+    val dragCustomView by lazy {
+        findViewById<DragCustomView>(R.id.dragCustomView)
+    }
+    val btnAddIcon by lazy {
+        findViewById<Button>(R.id.btnAddIcon)
+    }
+    val btnAddText by lazy {
+        findViewById<Button>(R.id.btnAddText)
+    }
+
+    val btnClear by lazy {
+        findViewById<Button>(R.id.btnClear)
+    }
+
+    val btnBack by lazy {
+        findViewById<Button>(R.id.btnBack)
+    }
+
+    val resources = listOf(
+        R.drawable.sticker_coffe_maker,
+        R.drawable.sticker_chef_cook_man,
+        R.drawable.sticker_ice_bucket_drink_ice,
+        R.drawable.sticker_ice_cream,
+        R.drawable.sticker_scissor_tool_school,
+        R.drawable.sticker_tsunami_waves,
+    )
+
+    val textos = listOf(
+        "Lorem Ipsum is simply dummy text of the printing and typesetting",
+        "Lorem Ipsum",
+        "Lorem Ipsum is simply",
+        "fwfewfw",
+        "-♥️-"
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnAddIcon.setOnClickListener {
+            val sticker =  BitmapFactory.decodeResource(getResources(), resources.random())
+            dragCustomView.addSticker(sticker)
+        }
+
+        btnAddText.setOnClickListener {
+            dragCustomView.addText(
+                textos.random()
+            )
+        }
+
+        btnClear.setOnClickListener {
+            dragCustomView.clearAll()
+        }
+
+        btnBack.setOnClickListener {
+            dragCustomView.goBack()
+        }
+//        dragCustomView.addText("HelloWorld")
+
+    }
+}
